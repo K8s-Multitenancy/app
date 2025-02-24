610ab156cc267f0c41ede4a221095dd91a462c47
dipa.alhaza <dipa.alhaza@ui.ac.id>
2025-02-24 20:40:27 +0700

    fix: minor update

--- hnc/script.sh
+++ hnc/script.sh
@@ -1,6 +1,6 @@
 kubectl apply -f db_config.yaml -n admin
 kubectl apply -f admin_config.yaml -n admin
-kubectl apply -f db_credentials.yaml -n admin
+kubectl apply -f credentials.yaml -n admin
 
 kubectl apply -f deployment.yaml -n admin
 kubectl apply -f service.yaml -n admin
@@ -9,7 +9,7 @@ kubectl apply -f service.yaml -n admin
 
 kubectl apply -f db_config.yaml -n tenant-a
 kubectl apply -f tenant_config.yaml -n tenant-a
-kubectl apply -f db_credentials.yaml -n tenant-a
+kubectl apply -f credentials.yaml -n tenant-a
 
 kubectl apply -f deployment.yaml -n tenant-a
 kubectl apply -f service.yaml -n tenant-a
